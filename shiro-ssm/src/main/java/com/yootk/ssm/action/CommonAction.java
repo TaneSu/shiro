ff5dddf090576772fdc268d65b80a691776ce6b3
admin <1240057191@qq.com>
2019-07-02 21:45:00 +0800

    控制层认证授权检测

--- shiro-ssm/src/main/java/com/yootk/ssm/action/CommonAction.java
+++ shiro-ssm/src/main/java/com/yootk/ssm/action/CommonAction.java
@@ -1,20 +1,30 @@
 package com.yootk.ssm.action;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonAction {
+
     @RequestMapping("/login")
     public String login() {
+
         return "login" ;
     }
+
+    @RequiresRoles("member")
+    @RequiresPermissions("dept:add")
     @RequestMapping("/pages/welcome")
     public String welcome() {
+
         return "welcome" ;
     }
+
     @RequestMapping("/logoutInfo")
     public String logout() {
+
         return "logout_info" ;
     }
 }
